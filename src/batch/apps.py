24440e84286c4a7f3f763090feb9800ad97ea270
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2024-07-18 20:39:54 +0900

    定期的に処理を実行できるようなものを作成

--- src/batch/apps.py
+++ src/batch/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class BatchConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'batch'
+
+    def ready(self):
+        from .management.commands.example_batch import start
+        start()
